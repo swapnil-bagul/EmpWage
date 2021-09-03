d76fe30e054ef37ab1c8ca0d4d1dc99fd7bbfbf0
swapnil-bagul <bagulswapnilp@gmail.com>
2021-09-03 14:00:20 +0530

    StoreDailyTotalWage

---
+++ StoreDailyTotalWage.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+isFullTime=1
+isPartTime=2
+isAbsent=0
+WagePerHr=20
+TotalWorkingday=20
+TotalWage=0
+Day=i
+declare -a DailyWage
+declare -a TotalWage
+for((i=1; i<=20; i++))
+
+do
+
+check=$((RANDOM%3))
+
+case $check in
+$isPartTime)	WorkingHr=4
+;;
+$isFullTime)	WorkingHr=8
+;;
+*)		WorkingHr=0
+;;
+esac
+		Wage=$(($WorkingHr*$WagePerHr))
+		DailyWage[i]="$Wage"	
+		TotalWage=$(($TotalWage+$Wage))
+		TotalWage[i]="$TotalWage"
+done
+		echo "Daily Wage is "${DailyWage[@]}" "
+		echo "Total wage is "${TotalWage[@]}" "
